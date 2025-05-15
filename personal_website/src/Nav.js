8b05f5e17633ef7d8327b1391f17461be466158b
gowthamnamuru <gowthamnamuri@gmail.com>
2025-05-16 00:30:28 +0530

    Make nav bar as responsive

--- personal_website/src/Nav.js
+++ personal_website/src/Nav.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
 
 function Nav() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [projectOpen, setProjectOpen] = useState(false);
     return (
-        <nav>
-          <a href="#home">Gowtham</a>
-          <div className="menu">
+        <nav className='navBar'>
+          <a href="#home" className='logo'>Gowtham</a>
+
+          <div className= {`menu ${menuOpen ? 'open' : ''}`}>
             <a href="#about">About</a>
-            <div className="projects_dropdown">
+
+            <div 
+            className="projects_dropdown"
+            onClick={() => setProjectOpen(!projectOpen)}
+                onMouseEnter={() => setProjectOpen(true)}
+                onMouseLeave={() => setProjectOpen(false)}
+            >
+
               <a href="#projects">Projects</a>
-              <span className="dropdown_icon"> ▾</span>
-              <div className="projects_dropdown_content">
+              <span className={`dropdown_icon ${projectOpen ? 'rotate' : ''}`}> ▾</span>
+              <div className={`projects_dropdown_content ${projectOpen ? 'show' : ''}`}>
                 <a href="#project1">American Express</a>
                 <a href="#project2">PayPal</a>
                 <a href="#project3">Kaiser Permanente</a>
@@ -21,6 +31,11 @@ function Nav() {
             </div>
             <a href="#contact">Contact</a>
           </div>
+
+        <div className="hamburger"
+         onClick={() => setMenuOpen(!menuOpen)}>
+            ☰
+        </div>
         </nav>
     )
 }
