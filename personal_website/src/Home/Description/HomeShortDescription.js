28899ff8de9ae741adaf48ecd79b17423822ed8f
gowthamnamuru <gowthamnamuri@gmail.com>
2025-05-16 12:42:02 +0530

    move ShortDescriptio to its own file

---
+++ personal_website/src/Home/Description/HomeShortDescription.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './HomeShortDescription.css';
+
+function HomeShortDescription() {
+    return (
+        <div className="content">
+            <h1>
+                Hey there,
+            </h1>
+            <p>
+                I am Gowtham Namuru - Senior iOS Engineer.
+                I always look for projects that challenge me and take me out of my comfort zone.
+            </p>
+            <a href="#" className="resume_button">See my resume</a>
+        </div>
+    )
+}
+
+export default HomeShortDescription;
--- personal_website/src/Home/Home.js
+++ personal_website/src/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Home.css';
 import Photo from "../Common/Photo";
 import Description from "./Description/Description";
+import HomeShortDescription from "./Description/HomeShortDescription";
 
 function Home() {
     return (
@@ -14,16 +15,7 @@ function Home() {
                     <div className="photo">
                     <Photo />
                     </div>
-                    <div className="content">
-                        <h1>
-                            Hey there,
-                        </h1>
-                        <p>
-                            I am Gowtham Namuru - Senior iOS Engineer.
-                            I always look for projects that challenge me and take me out of my comfort zone.
-                        </p>
-                        <a href="#" className="resume_button">See my resume</a>
-                    </div>
+                    <HomeShortDescription />
                 </div>
             </section>
         </div>
